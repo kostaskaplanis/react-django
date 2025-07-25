7e03aa61efc62a312efd2fd3f6110c5a6c6c2ba4
Kostas Kaplanis <kaplanis@thepixelocracy.com>
2025-07-25 17:29:14 +0300

    update book title from the client side

--- client/app/src/App.jsx
+++ client/app/src/App.jsx
@@ -7,6 +7,7 @@ function App() {
   const [books, setBooks] = useState([]);
   const [title, setTitle] = useState("");
   const [releaseYear, setReleaseYear] = useState("");
+  const [newTitle, setNewTitle] = useState("");
 
   useEffect(() => {
     const fetchBooks = async () => {
@@ -42,6 +43,35 @@ function App() {
     }
   };
 
+  const updateTitle = async (pk, release_year) => {
+    const bookData = {
+      title: newTitle,
+      release_year,
+    };
+
+    try {
+      const response = await fetch(`http://127.0.0.1:8000/api/books/${pk}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(bookData),
+      });
+      const data = await response.json();
+      setBooks((prev) =>
+        prev.map((book) => {
+          if (book.id === pk) {
+            return data;
+          } else {
+            return book;
+          }
+        })
+      );
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
   return (
     <>
       <h1>Book Website</h1>
@@ -63,6 +93,14 @@ function App() {
         <div>
           <p>Title: {book.title}</p>
           <p>Release Year: {book.release_year}</p>
+          <input
+            type="text"
+            placeholder="New Title"
+            onChange={(e) => setNewTitle(e.target.value)}
+          />
+          <button onClick={() => updateTitle(book.id, book.release_year)}>
+            Change Title
+          </button>
         </div>
       ))}
     </>
