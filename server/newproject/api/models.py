bb63288730b495a4a48ddb175cbdf3609c6ba885
Kostas Kaplanis <kaplanis@thepixelocracy.com>
2025-07-25 16:36:32 +0300

    Books models small fix , create_book view,  migrations

--- server/newproject/api/models.py
+++ server/newproject/api/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Book(models.Model): 
-    book_title = models.CharField(max_length=50),
+    title = models.CharField(max_length=50)
     release_year = models.IntegerField()
     
     def __str__(self): 
