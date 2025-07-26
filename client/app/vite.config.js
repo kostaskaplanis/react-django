cfd788041abc70a9e7994682b357a53ffe638f53
Konstantinos Kaplanis <contact@kostaskaplanis.gr>
2025-07-26 19:53:31 +0300

    docker final fixes

--- client/app/vite.config.js
+++ client/app/vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: "0.0.0.0", // ← allow external connections
+    port: 5173,
+    strictPort: true, // optional: crash instead of choosing a random port
+    watch: {
+      usePolling: true, // good for Docker volume changes
+    },
+  },
+});
